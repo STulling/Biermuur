b26dc43013e95cb039773ccdc556bbf9a44466f8
dion <dionrovers10@gmail.com>
2021-05-26 10:33:28 +0200

    Add regenboog

--- display.py
+++ display.py
@@ -64,6 +64,34 @@ def movingText(text, speed):
         time.sleep(speed)
 
 
+def rainbow(wait_ms=20, iterations=1):
+    """Draw rainbow that fades across all pixels at once."""
+    for j in range(256*iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel((i+j) & 255))
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+def rainbowCycle(wait_ms=20, iterations=5):
+    """Draw rainbow that uniformly distributes itself across all pixels."""
+    for j in range(256*iterations):
+        for i in range(strip.numPixels()):
+            strip.setPixelColor(i, wheel((int(i * 256 / strip.numPixels()) + j) & 255))
+        strip.show()
+        time.sleep(wait_ms/1000.0)
+
+def theaterChaseRainbow(wait_ms=50):
+    """Rainbow movie theater light style chaser animation."""
+    for j in range(256):
+        for q in range(3):
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, wheel((i+j) % 255))
+            strip.show()
+            time.sleep(wait_ms/1000.0)
+            for i in range(0, strip.numPixels(), 3):
+                strip.setPixelColor(i+q, 0)
+
+
 def init():
     global strip
     # Create NeoPixel object with appropriate configuration.
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from display import init, movingText, setStrip
+import display
 
 app = Flask(__name__)
 
@@ -10,6 +11,8 @@ def index():
         if request.form.get('clear'):
             print("Clearing")
             setStrip((0, 0, 0))
+        elif request.form.get('regenboog'):
+            display.rainbow()
         elif request.form.get('show') and request.form.get('text'):
             print("showing: " + request.form.get('text'))
             movingText(request.form.get('text'), 0.04)
