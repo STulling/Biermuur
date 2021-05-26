c19f6b4f4790a35df2f138870165252fa58bbabd
Simon Tulling <simon.tulling99@gmail.com>
2021-05-26 18:33:47 +0200

    Quicker fix

--- display.py
+++ display.py
@@ -36,7 +36,8 @@ def getHTMLColors():
 
 def RGBToHTMLColor(rgb_tuple):
     """ convert an (R, G, B) tuple to #RRGGBB """
-    hexcolor = '#%02x%02x%02x' % rgb_tuple
+    newrgb = (rgb_tuple[1], rgb_tuple[0], rgb_tuple[2])
+    hexcolor = '#%02x%02x%02x' % newrgb
     # that's it! '%02x' means zero-padded, 2-digit hex values
     return hexcolor
 
