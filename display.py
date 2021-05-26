2a70942db2f2d6b49019b0e3a164cc750913b814
Simon Tulling <simon.tulling99@gmail.com>
2021-05-26 13:52:25 +0200

    fix?

--- display.py
+++ display.py
@@ -137,10 +137,12 @@ def golf():
     xs = [2 * np.pi * x / 11 for x in range(12)]
     t = 0
     dt = 0.01
+    color = Color(0, 255, 255)
+    print(color)
     while True:
         t += dt
         ys = [int(6 * np.sin(x + t) + 6) for x in xs]
         setStrip((255, 0, 0), False)
         for x, y in zip(xs, ys):
-            setPixelColor(x, y, getIfromRGB((0, 255, 255)))
+            setPixelColor(x, y, color)
         strip.show()
