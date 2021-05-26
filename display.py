3178619a8908449a0fc86b8b2c7583aceaf38972
SkullOwner <skull.onaar@gmail.com>
2021-05-26 12:54:42 +0200

    Add delay needed for display

--- display.py
+++ display.py
@@ -126,6 +126,7 @@ def shapewipe(shape=None, color=(255, 255, 0)):
         sizedshape = [(i[0] * x + WIDTH / 2 - 1, i[1] * x + HEIGHT / 2 - 1) for i in shape]
         d.polygon(xy=sizedshape, fill=color, outline=color)
         show(out)
+        time.sleep(wait_ms / 1000.0)
 
 
 def diamondwipe(color=(255, 255, 0)):
