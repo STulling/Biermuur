df03cebba289c7ee1c42e4fcf948ca301c8a84e4
Simon Tulling <simon.tulling99@gmail.com>
2021-05-26 18:01:29 +0200

    Fix rainbow

--- display.py
+++ display.py
@@ -84,13 +84,13 @@ def movingText(text, speed, loop=False):
 def wheel(pos):
     """Generate rainbow colors across 0-255 positions."""
     if pos < 85:
-        return (pos * 3, 255 - pos * 3, 0)
+        return Color(pos * 3, 255 - pos * 3, 0)
     elif pos < 170:
         pos -= 85
-        return (255 - pos * 3, 0, pos * 3)
+        return Color(255 - pos * 3, 0, pos * 3)
     else:
         pos -= 170
-        return (0, pos * 3, 255 - pos * 3)
+        return Color(0, pos * 3, 255 - pos * 3)
 
 
 def rainbow(wait_ms=20, iterations=1):
