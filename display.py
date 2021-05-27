9d5e1bceea9d36a823d9273c2c9e75e67bb86631
dion <dionrovers10@gmail.com>
2021-05-27 14:09:21 +0200

    yaaaaa

--- display.py
+++ display.py
@@ -335,14 +335,14 @@ def spiraal():
         theta = 2*np.pi
         x = 6
         y = 6
+        setStrip(secondary, False)
         while r < 8:
-            setStrip(secondary, False)
-            theta += 0.2*np.pi
-            r+= 0.1
+            theta += 0.05*np.pi
+            r+= 0.05
             x = int(r*np.cos(theta)) + 6
             y = int(r*np.sin(theta)) + 6
             print(x, y)
             setPixelColor(x,y, primary)
             strip.show()
-            time.sleep(0.2)
+            time.sleep(0.05)
 
