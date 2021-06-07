0e3e029e376bc17c5ef8660faf88c50dd715d942
Simon Tulling <simon.tulling99@gmail.com>
2021-06-07 11:55:41 +0200

    More leds

--- display.py
+++ display.py
@@ -12,7 +12,7 @@ import random
 from fake_strip import FakeStrip
 
 # LED strip configuration:
-LED_COUNT = 400  # Number of LED pixels.
+LED_COUNT = 360  # Number of LED pixels.
 LED_PIN = 18  # GPIO pin connected to the pixels (18 uses PWM!).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10  # DMA channel to use for generating signal (try 10)
@@ -20,7 +20,7 @@ LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
 LED_INVERT = False  # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL = 0  # set to '1' for GPIOs 13, 19, 41, 45 or 53
 WIDTH = 20
-HEIGHT = 20
+HEIGHT = 18
 strip = FakeStrip()
 
 fnt = ImageFont.truetype("Pixel12x10Mono.ttf", 13)
