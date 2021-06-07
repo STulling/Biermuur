6256564359fc407b0e60cd97700ebc223caa3e3e
Simon Tulling <simon.tulling99@gmail.com>
2021-06-07 23:39:02 +0200

    SPI pls

--- display.py
+++ display.py
@@ -13,7 +13,7 @@ import random
 
 # LED strip configuration:
 LED_COUNT = 360  # Number of LED pixels.
-LED_PIN = 21  # GPIO pin connected to the pixels (18 uses PWM!).
+LED_PIN = 10  # GPIO pin connected to the pixels (18 uses PWM!).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA = 10  # DMA channel to use for generating signal (try 10)
 LED_BRIGHTNESS = 255  # Set to 0 for darkest and 255 for brightest
