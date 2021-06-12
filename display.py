Fix flipping of wrong axis

display.py
@@ -391,10 +391,10 @@ def boxes():
         kraty = random.randint(0, N_KRAT_Y-1)
         xval += kratx * KRAT_WIDTH
         yval += kraty * KRAT_HEIGHT
+        if bool(random.getrandbits(1)):
+            yval = np.flip(yval)
         for y in yval:
             time.sleep(0.1)
-            if bool(random.getrandbits(1)):
-                xval = np.flip(xval)
             for x in xval:
                 setPixelColor(x, y, primary)
                 strip.show()