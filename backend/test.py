eb181d11078bbed797cfd773c9f8c526297b2a5c
Simon Tulling <simon.tulling99@gmail.com>
2021-06-28 14:29:02 +0200

    pjer part dos

--- backend/test.py
+++ backend/test.py
@@ -7,8 +7,8 @@ import sys
 import DJ
 
 def main():
-    music.folder = '.'
-    DJ.loop('Benee')
+    music.folder = '/media/pi/F/music'
+    DJ.loop('Elderbrook - Bird Song')
 
 if __name__=="__main__":
     main()
