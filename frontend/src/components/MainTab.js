3e8f3231dca079300985820ad290237704db89bf
Simon Tulling <simon.tulling99@gmail.com>
2021-06-30 17:27:12 +0200

    fix imports

--- frontend/src/components/MainTab.js
+++ frontend/src/components/MainTab.js
@@ -6,14 +6,10 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListSubheader from '@material-ui/core/ListSubheader';
-import Divider from '@material-ui/core/Divider';
 import TextFieldsIcon from '@material-ui/icons/TextFields';
-import IconButton from '@material-ui/core/IconButton';
 import Button from '@material-ui/core/Button';
 import SendIcon from '@material-ui/icons/Send';
-import WifiIcon from '@material-ui/icons/Wifi';
 import CasinoIcon from '@material-ui/icons/Casino';
-import BluetoothIcon from '@material-ui/icons/Bluetooth';
 import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles((theme) => ({
--- frontend/src/components/SettingsTab.js
+++ frontend/src/components/SettingsTab.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListSubheader from '@material-ui/core/ListSubheader';
-import Divider from '@material-ui/core/Divider';
-import TextFieldsIcon from '@material-ui/icons/TextFields';
-import IconButton from '@material-ui/core/IconButton';
 import SendIcon from '@material-ui/icons/Send';
-import WifiIcon from '@material-ui/icons/Wifi';
-import CasinoIcon from '@material-ui/icons/Casino';
-import BluetoothIcon from '@material-ui/icons/Bluetooth';
 import TextField from '@material-ui/core/TextField';
 import PaletteIcon from '@material-ui/icons/Palette';
 import { ColorPicker } from 'material-ui-color';
