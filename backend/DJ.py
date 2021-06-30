5354aee88ccc7e1efca1e437093d7cf75dd0d4d6
Simon Tulling <simon.tulling99@gmail.com>
2021-07-01 00:25:46 +0200

    remove keyboard

--- backend/DJ.py
+++ backend/DJ.py
@@ -1,5 +1,3 @@
-import keyboard
-
 import music
 import AudioUtils
 import os
@@ -21,30 +19,12 @@ mPlayer = None
 currentCallback = multiprocessing.Value('i', 0)
 
 
-def on_press(key):
-    if key.name == 'esc':
-        currentCallback.value = callbackNames.index('quit')
-    if key.name == 'b':
-        currentCallback.value = callbackNames.index('bars')
-    if key.name == 's':
-        currentCallback.value = callbackNames.index('sparkle')
-    if key.name == 'r':
-        currentCallback.value = callbackNames.index('diamond')
-    if key.name == 'c':
-        currentCallback.value = callbackNames.index('circle')
-    if key.name == 'w':
-        currentCallback.value = callbackNames.index('wave')
-    if key.name == 'l':
-        currentCallback.value = callbackNames.index('lightning')
-
-
 def callback(rms):
     callbackFunctions[currentCallback.value](rms)
 
 
 def loop(file):
     file = os.path.join(music.folder, file + '.wav')
-    keyboard.on_press(on_press)
     global mPlayer
     mPlayer = music.MusicPlayer(callback_function=callback)
     mPlayer.playSound(file)
