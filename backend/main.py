076c7e8aceeb73424923781cc142a4fb077433bf
Simon Tulling <simon.tulling99@gmail.com>
2021-08-12 00:33:41 +0200

    cronge

--- backend/main.py
+++ backend/main.py
@@ -40,7 +40,7 @@ def setAction(action, args):
 
 class Songs(Resource):
     def get(self):
-        return make_response(json.dumps(music.listSongs()))
+        return make_response(json.dumps(music.listSongs()).decode('utf-8'))
 
 class Play(Resource):
     def get(self, song_name):
