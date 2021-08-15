e1811337a48179d0eab1ecc17cff7c4ce41319ac
Simon Tulling <simon.tulling99@gmail.com>
2021-08-15 12:20:59 +0200

    bokito power

--- backend/music.py
+++ backend/music.py
@@ -28,7 +28,7 @@ def listFolders():
     return [x[0] for x in os.walk(folder)]
 
 def listSongs():
-    return [f[:-4] for f in os.listdir(folder) if f.endswith('.mp3')]
+    return [f[:-4] for f in os.listdir(folder) if f.endswith(b'.mp3')]
 
 
 class MusicPlayer():
